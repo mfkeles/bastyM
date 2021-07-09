ce2dcb3c7efe29f2e1786509384c23f040ead30f
mfkeles <mkeles@ucla.edu>
2021-07-09 16:09:36 -0400

    docker update

--- utils/initDockerDLC.sh
+++ utils/initDockerDLC.sh
@@ -7,3 +7,5 @@ sudo mount -t nfs 10.17.6.43:/volume1/Lab_Files /mnt/wunas
 
 
 docker run --gpus device=0 -it tensorflow/tensorflow:latest-gpu bash
+
+docker run --gpus device=1 -v /home/grover/Desktop/dockerdir/LiquidFoodDLC-Mehmet-2020-03-23/:/mnt/dlcdata -it --rm dlctf2 /bin/bash
