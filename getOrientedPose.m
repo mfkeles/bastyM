4925d6219f8ee1b21eddd5fad0caa50f262c800f
mfkeles <mkeles@ucla.edu>
2021-08-18 16:49:40 -0400

    fixed yaml

--- getOrientedPose.m
+++ getOrientedPose.m
@@ -5,7 +5,17 @@ llh = getSubCoord(obj,'likelihood');
 x = getSubCoord(obj,'x');
 y = getSubCoord(obj,'y');
 
+counterparts = obj.pose_cfg.("counterparts");
+connected_parts = obj.pose_cfg.("connected_parts");
 
+names = fieldnames(counterparts);
+for i = 1:numel(names)
+    left_part = counterparts.(names{i}){1};
+    right_part = counterparts.(names{i}){2};
+    
+    connected_left = 
+    
+end
 function subData = getSubCoord(obj,ind)
 
 IndexC = strfind(obj.Data.Properties.VariableUnits,ind);
@@ -14,3 +24,7 @@ subData = obj.Data(:,Index);
 subData.Properties.VariableNames = obj.Data.Properties.VariableDescriptions(Index);
 
 end
+
+
+end
+
