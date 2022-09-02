394130ce379e5b436d2b04e8c2c03b4b46760ca9
mfkeles <mkeles@ucla.edu>
2022-09-02 12:11:53 -0400

    daily

--- scripts/plotExampleBehaviorData.m
+++ scripts/plotExampleBehaviorData.m
@@ -20,3 +20,86 @@ annotFile = 'FlyF15-08182021173222.csv';
 
 behaviorT = readtable(fullfile(folderPath,annotFile));
 
+
+[G behavior] = findgroups(behaviorT.Behavior);
+
+
+function plotXYTrace(behaviorT,dfPose,behaviorName,bodyPart,folderPath)
+%prob pumps
+behIdx = behaviorT{ismember(behaviorT.Behavior,behaviorName),2:3};
+
+bodyPart_x = strcat(bodyPart,'_x');
+bodyPart_y = strcat(bodyPart,'_y');
+
+targetFolder = fullfile(folderPath,behaviorName);
+
+
+
+if ~isfolder(targetFolder)
+    mkdir(targetFolder)
+end
+
+clf
+figure(1)
+Plotter.initiatePlot(12,'w','Myriad Pro')
+h=gcf;
+hx = plot(0,0,'LineWidth',1);
+hold all
+hy = plot(0,0,'LineWidth',1);
+hold off
+
+hGCA = get(gca);
+hGCA.XLabel.String = 'Time (sec)';
+hLegend = legend(bodyPart_x,bodyPart_y);
+hLegend.Interpreter = 'none';
+
+
+for i=1:size(behIdx,1)
+    hx.YData = dfPose.(bodyPart_x)(behIdx(i,1):behIdx(i,2));
+    hx.XData = 1:length(behIdx(i,1):behIdx(i,2));
+    hy.YData = dfPose.(bodyPart_y)(behIdx(i,1):behIdx(i,2));
+    hy.XData = 1:length(behIdx(i,1):behIdx(i,2));
+    cticks = xticks;
+    xticks(0:60:max(cticks));
+    xticklabels(0:2:max(cticks)/30)
+    tString = strcat(behaviorName,'_',num2str(behIdx(i,1)),'-',num2str(behIdx(i,2)));
+    title(tString,'Interpreter','none')
+    exportgraphics(gcf,fullfile(targetFolder,strcat(tString,'.pdf')),'Resolution',300,'ContentType','vector');
+    saveas(gcf,fullfile(targetFolder,strcat(tString,'.svg')));
+    savefig(gcf,fullfile(targetFolder,strcat(tString,'.fig')))
+end
+
+
+clf
+figure(1)
+Plotter.initiatePlot(12,'w','Myriad Pro')
+
+
+hGCA = get(gca);
+hGCA.XLabel.String = 'Time (sec)';
+hLegend = legend(bodyPart_x,bodyPart_y);
+hLegend.Interpreter = 'none';
+
+varNames = dfPose.Properties.VariableNames;
+
+%plot all parts
+for j = 1:numel(varNames)-2
+    for i=1:size(behIdx,1) %bodyPart_x 
+        h = plot(dfPose.(varNames{j})(behIdx(i,1):behIdx(i,2)));
+        hold on
+        hx.XData = 1:length(behIdx(i,1):behIdx(i,2));
+        hy.YData = dfPose.(varNames{j})(behIdx(i,1):behIdx(i,2));
+        hy.XData = 1:length(behIdx(i,1):behIdx(i,2));
+        cticks = xticks;
+        xticks(0:60:max(cticks));
+        xticklabels(0:2:max(cticks)/30)
+        tString = strcat(behaviorName,'_',num2str(behIdx(i,1)),'-',num2str(behIdx(i,2)));
+        title(tString,'Interpreter','none')
+        exportgraphics(gcf,fullfile(targetFolder,strcat(tString,'.pdf')),'Resolution',300,'ContentType','vector');
+        saveas(gcf,fullfile(targetFolder,strcat(tString,'.svg')));
+        savefig(gcf,fullfile(targetFolder,strcat(tString,'.fig')))
+    end
+end
+
+
+
