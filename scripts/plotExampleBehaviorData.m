402be0dd14a33fb401b4aeb631819345bcd62ab6
mfkeles <mkeles@ucla.edu>
2022-09-06 11:03:20 -0400

    Update plotExampleBehaviorData.m

--- scripts/plotExampleBehaviorData.m
+++ scripts/plotExampleBehaviorData.m
@@ -70,35 +70,49 @@ for i=1:size(behIdx,1)
 end
 
 
-clf
-figure(1)
-Plotter.initiatePlot(12,'w','Myriad Pro')
-
-
-hGCA = get(gca);
-hGCA.XLabel.String = 'Time (sec)';
-hLegend = legend(bodyPart_x,bodyPart_y);
-hLegend.Interpreter = 'none';
-
-varNames = dfPose.Properties.VariableNames;
 
 %plot all parts
-for j = 1:numel(varNames)-2
-    for i=1:size(behIdx,1) %bodyPart_x 
-        h = plot(dfPose.(varNames{j})(behIdx(i,1):behIdx(i,2)));
+for i=1:size(behIdx,1) %bodyPart_x
+
+    clf
+    figure(1)
+    Plotter.initiatePlot(12,'w','Myriad Pro')
+    h = plot(0,0);
+    hold on
+    hGCA = get(gca);
+    hGCA.XLabel.String = 'Time (sec)';
+
+    varNames = dfPose.Properties.VariableNames;
+
+    cmap = brewermap(42,'Spectral');
+    for j=1:numel(varNames)-2
+        h = plot(dfPose.(varNames{j})(behIdx(i,1):behIdx(i,2)),'LineWidth',1);
         hold on
-        hx.XData = 1:length(behIdx(i,1):behIdx(i,2));
-        hy.YData = dfPose.(varNames{j})(behIdx(i,1):behIdx(i,2));
-        hy.XData = 1:length(behIdx(i,1):behIdx(i,2));
+        h.XData = 1:length(behIdx(i,1):behIdx(i,2));
+        h.YData = dfPose.(varNames{j})(behIdx(i,1):behIdx(i,2));
+        h.Color = cmap(j,:);
         cticks = xticks;
         xticks(0:60:max(cticks));
         xticklabels(0:2:max(cticks)/30)
         tString = strcat(behaviorName,'_',num2str(behIdx(i,1)),'-',num2str(behIdx(i,2)));
         title(tString,'Interpreter','none')
-        exportgraphics(gcf,fullfile(targetFolder,strcat(tString,'.pdf')),'Resolution',300,'ContentType','vector');
-        saveas(gcf,fullfile(targetFolder,strcat(tString,'.svg')));
-        savefig(gcf,fullfile(targetFolder,strcat(tString,'.fig')))
+        tString=strcat(tString,'fullset');
+        cbar = 1;
+        %create the colorbar
+        if cbar
+            colormap(cmap);
+            ch = colorbar;
+            ch.Ticks = 0.0119:0.0238:1;
+            ch.TickLabels = varNames;
+            ch.TickLabelInterpreter = 'none';
+            ch.FontName = 'Myriad Pro';
+        end
     end
+
+    exportgraphics(gcf,fullfile(targetFolder,strcat(tString,'.pdf')),'Resolution',300,'ContentType','vector');
+    saveas(gcf,fullfile(targetFolder,strcat(tString,'.svg')));
+    savefig(gcf,fullfile(targetFolder,strcat(tString,'.fig')))
+end
 end
 
 
